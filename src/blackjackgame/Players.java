6e5645a8826d000f950efd822ebb7ea6f1284515
DanisHack <danishmohd3610@gmail.com>
2014-07-05 16:39:19 -0700

    progressing

--- src/blackjackgame/Players.java
+++ src/blackjackgame/Players.java
@@ -3,7 +3,65 @@ package blackjackgame;
 public class Players {
 
 	private String playerName;
-	private Cards[] playerHand;
+	private Cards[] playerHand = new Cards[10];
+	private int numCardsInHand;
+	
+	
+	public Players(String name){
+		
+		this.playerName = name;
+		
+		this.emptyHand();
+	}
+	
+	public void emptyHand(){
+		
+		for(int hc=0; hc<10;hc++){
+			this.playerHand[hc] = null;
+		}
+		
+		this.numCardsInHand = 0;
+		
+	}
+	
+	public boolean addCardToPlayersHand(Cards card){
+		
+		if(this.numCardsInHand == 10){
+			System.err.printf("%s's hand already has 10 cards; cannot add more cards", this.playerName);
+			System.exit(1);
+		}
+		
+		this.playerHand[this.numCardsInHand] = card;
+		this.numCardsInHand++;
+		
+		return (this.getPlayersHandTotal() <=21);
+		
+	}
+	
+	public int getPlayersHandTotal(){
+		
+		int handTotal = 0;
+		int cardNum;
+		int numAces = 0;
+		
+		for(int c =0; c<this.numCardsInHand;c++){
+			
+			cardNum = this.playerHand[c].getCardNumber();
+			
+			if(cardNum == 1){ // Ace
+				numAces++;
+				handTotal += 11;
+			}
+			else if(cardNum >= 10){
+				handTotal += 10;
+			}
+			else{
+				handTotal += cardNum;
+			}
+		}
+		return 1;
+	}
+	
 	
 	
 }
