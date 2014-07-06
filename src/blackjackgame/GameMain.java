8188fae8feace2954e085e5c6434df975eff5ccf
DanisHack <danishmohd3610@gmail.com>
2014-07-05 17:10:55 -0700

    main game play

--- src/blackjackgame/GameMain.java
+++ src/blackjackgame/GameMain.java
@@ -6,12 +6,22 @@ package blackjackgame;
  *
  */
 
+import java.util.Scanner;
+
 public class GameMain {
 
 	public static void main(String[] args) {
 		
+		//game init
+		Scanner sc = new Scanner(System.in);
 		Deck newDeck = new Deck(2, true);
-		newDeck.printDeckCards(10);
+		
+		
+		//players init
+		Players you = new Players("Danish Mohd");
+		Players dealer = new Players("Dealer");
+		
+		
 
 	}
 
